e9599e2e68f8d1c97aefb9962787c3c5003c3e30
Kha Sang <sang.khask12@hcmut.edu.vn>
2022-10-11 22:59:21 +0700

    first upload

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+import React, {useEffect, useState} from 'react'
+
+function App() {
+  const [data,setData] = useState([{}])
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/api/').then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+      }
+    )
+  },[])
+
+  return (
+    <div className="App">
+      {
+        (typeof data.msg === 'undefined')?(
+          <p>Loading...</p>
+        ):(
+          data.msg.map((user,i)=>(
+            <p key={i}>{user}</p>
+          ))
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/api',(req,res)=>{
+    res.json({"msg":["A","B","C"]})
+})
+
+app.listen(5000,()=>{console.log("Server started on 5000");})
