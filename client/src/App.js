c8fe398ffb2fef0fe868d4b6c63a07af2be33c03
duy1711ak <duy.pham1711a@hcmut.edu.vn>
2022-10-17 20:42:24 +0700

    init client

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,22 @@
 import './App.css';
 
-import React, {useEffect, useState} from 'react'
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import InitPage from "./components/pages/initPage";
 
-function App() {
-  const [data,setData] = useState([{}])
-
-  useEffect(()=>{
-    fetch('http://localhost:5000/api/').then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-      }
-    )
-  },[])
-
+function App () {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <InitPage/>
+    } // Add new page here
+  ]);
   return (
-    <div className="App">
-      {
-        (typeof data.msg === 'undefined')?(
-          <p>Loading...</p>
-        ):(
-          data.msg.map((user,i)=>(
-            <p className="text-4xl font-bold underline" key={i}>{user}</p>
-          ))
-        )
-      }
-    </div>
-  );
+    <RouterProvider router={router}></RouterProvider>
+  )
 }
 
 export default App;
