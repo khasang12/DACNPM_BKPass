81e4717e8482802a77df454a3b9ee499b6f9ff26
Kha Sang <sang.khask12@hcmut.edu.vn>
2022-10-18 20:43:49 +0700

    display item done

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddItemForm from "./components/pages/AddItemForm";
+import DisplayItem from "./components/pages/DisplayItem";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="add-item" element={<AddItemForm />} />
+        <Route path="demo-item" element={<DisplayItem />} />
       </Routes>
     </BrowserRouter>
   );
