e70db0b5ccc37616ad74b0972934815103cdfa95
duy1711ak <duy.pham1711a@hcmut.edu.vn>
2022-10-18 16:53:58 +0700

    Add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-import React, { useEffect, useState } from 'react'
-
-function App() {
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch('http://localhost:5000/api/').then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-      }
-    )
-  }, [])
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import InitPage from "./components/pages/initPage";
 
 function App () {
   const router = createBrowserRouter([
@@ -23,15 +15,7 @@ function App () {
   ]);
   return (
     <div className="App">
-      {
-        (typeof data.msg === 'undefined') ? (
-          <p>Loading...</p>
-        ) : (
-          data.msg.map((user, i) => (
-            <p className="text-4xl font-bold underline" key={i}>{user}</p>
-          ))
-        )
-      }
+      <RouterProvider router={router}/>
     </div>
   );
 }
