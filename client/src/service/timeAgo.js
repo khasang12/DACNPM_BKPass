80202a7ecca2064fa24f73ab97fd80bd7d9a2eb1
duy1711ak <duy.pham1711a@hcmut.edu.vn>
2022-10-20 23:20:11 +0700

    Add search page

---
+++ client/src/service/timeAgo.js
@@ -0,0 +1,24 @@
+export function timeAgo (date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = seconds / 31536000;
+    if (interval > 1) {
+        return Math.floor(interval) + " năm trước";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+        return Math.floor(interval) + " tháng trước";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+        return Math.floor(interval) + " ngày trước";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+        return Math.floor(interval) + " giờ trước";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+        return Math.floor(interval) + " phút trước";
+    }
+    return "Vài giây trước";
+}
