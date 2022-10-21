8876081cc20d378b5d5f23dd3ae41aefacc910d3
Kha Sang <sang.khask12@hcmut.edu.vn>
2022-10-21 12:09:28 +0700

    fix minor bugs, create DOM event for forms

--- client/src/components/pages/AddItemForm.js
+++ client/src/components/pages/AddItemForm.js
@@ -7,11 +7,11 @@ export default function AddItemForm() {
   const [values, setValues] = useState({
     category: "",
     item_status: "",
-    price: 0,
+    price: "",
     header: "",
     desc: "",
     addr: "",
-    img: "",
+    img: [],
   });
 
   // FOR FILES UPLOADING....
@@ -21,6 +21,7 @@ export default function AddItemForm() {
     const files = e.target.files;
     const arr = Array.from(files);
     const arrItem = arr.map((file) => URL.createObjectURL(file));
+    setValues({ ...values, img: [arrItem] });
     await setFiles(arrItem);
   }
 
@@ -49,6 +50,7 @@ export default function AddItemForm() {
           navigate("/setAvatar");
         }
       }*/
+      console.log(values);
     }
   };
   const handleChange = (event) => {
@@ -58,53 +60,62 @@ export default function AddItemForm() {
     });
   };
   const handleValidation = () => {
-    /*const { password, username } = values;
+    const { price, img, category, item_status } = values;
     const toastOptions = {
       position: "top-right",
-      autoClose: 5000,
+      autoClose: 3000,
       pauseOnHover: true,
       draggable: true,
       theme: "dark",
     };
-    if (password === "") {
-      toast.error("Password required", toastOptions);
+    if (category == "Chọn danh mục" || category == "") {
+      toast.error("Vui lòng chọn danh mục", toastOptions);
       return false;
-    } else if (username === "") {
-      toast.info("Username required", toastOptions);
+    } else if (item_status == "") {
+      toast.error(
+        "Vui lòng chọn trạng thái hàng (Mới/Đã sử dụng)",
+        toastOptions
+      );
+      return false;
+    } else if (!price.match(/^\d+(?:[,]\d+)*VND$/)) {
+      toast.error("Giá tiền không thể là số âm", toastOptions);
+      return false;
+    } else if (img.length == 0) {
+      toast.error("Vui lòng thêm ít nhất 1 ảnh", toastOptions);
       return false;
     } else {
       return true;
-    }*/
+    }
   };
 
   return (
-    <div class="flex bg-gray-300 py-5 px-40">
-      <div class="flex-auto justify-center items-center text-gray-700 bg-white px-4 py-4 my-2 mx-0">
-        <div class="flex flex-col grow-0 justify-center items-center px-2">
+    <div className="flex bg-gray-300 px-40 border">
+      <div className="flex-auto justify-center items-center text-gray-700 bg-white px-4 py-7 mx-0">
+        <div className="flex flex-col grow-0 justify-center items-center px-2">
           <label
-            for="dropzone-file"
-            class="flex flex-col justify-center items-center px-2 w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer hover:bg-gray-100"
+            htmlFor="dropzone-file"
+            className="flex flex-col justify-center items-center px-2 w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer hover:bg-gray-100"
           >
-            <div class="flex flex-col justify-center items-center pt-5 pb-6">
+            <div className="flex flex-col justify-center items-center pt-5 pb-6">
               <svg
                 aria-hidden="true"
-                class="mb-3 w-10 h-10 text-gray-400"
+                className="mb-3 w-10 h-10 text-gray-400"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
                 ></path>
               </svg>
-              <p class="mb-2 text-sm text-gray-500">
-                <span class="font-semibold">Thêm hình ảnh</span> (ấn hoặc kéo)
+              <p className="mb-2 text-sm text-gray-500">
+                <span className="font-semibold">Thêm hình ảnh</span>
               </p>
-              <p class="text-xs text-gray-500">
+              <p className="text-xs text-gray-500">
                 SVG, PNG, JPG or GIF (MAX. 800x400px)
               </p>
             </div>
@@ -112,8 +123,9 @@ export default function AddItemForm() {
             <input
               id="dropzone-file"
               type="file"
-              class="hidden"
+              className="hidden"
               onChange={uploadMultipleFiles}
+              accept="image/*"
               multiple
             />
           </label>
@@ -125,23 +137,25 @@ export default function AddItemForm() {
         </div>
       </div>
 
-      <div class="shrink-0 w-1/2 text-gray-700 bg-white px-4 py-4 my-2">
+      <div className="shrink-0 w-2/3 text-gray-700 bg-white px-4 py-7">
         <form
           action=""
-          className="w-full max-w-lg"
+          className="w-full"
           onSubmit={(event) => handleSubmit(event)}
         >
-          <div class="inline-block relative w-64">
+          <div className="inline-block relative w-full">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
               htmlFor="grid-first-name"
             >
               Danh mục tin đăng
             </label>
             <select
+              name="category"
               onChange={(e) => handleChange(e)}
               className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
             >
+              <option>Chọn danh mục</option>
               <option>Sách</option>
               <option>Thiết bị số</option>
               <option>Vật dụng khác</option>
@@ -151,43 +165,43 @@ export default function AddItemForm() {
           <h1 className="text-2xl mt-10 mb-5 font-bold">Thông tin chi tiết</h1>
 
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-first-name"
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-first-name"
           >
             Tình trạng hàng
           </label>
-          <ul className="grid gap-6 w-full mt-3 mb-5 md:grid-cols-2 mt-2">
-            <li>
+          <ul className="flex w-2/3 mt-3 mb-5 md:grid-cols-2 mt-2">
+            <li className="flex-1">
               <input
                 type="radio"
                 id="status-new"
-                name="status"
+                name="item_status"
                 value="new"
                 className="hidden peer"
                 onChange={(e) => handleChange(e)}
                 required
               />
               <label
-                for="status-new"
-                className="inline-flex justify-between items-center p-2 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
+                htmlFor="status-new"
+                className="inline-flex justify-center items-center p-2 w-1/2 text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
               >
                 <div className="block">
-                  <div className="w-full">Còn mới</div>
+                  <div className="w-full">Hàng mới</div>
                 </div>
               </label>
             </li>
-            <li>
+            <li className="flex-1">
               <input
                 type="radio"
                 id="status-old"
-                name="status"
+                name="item_status"
                 value="old"
                 onChange={(e) => handleChange(e)}
                 className="hidden peer"
               />
               <label
-                for="status-old"
-                className="inline-flex justify-between items-center p-2 w-full text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
+                htmlFor="status-old"
+                className="inline-flex justify-center items-center p-2 w-1/2 text-gray-500 bg-white rounded-lg border border-gray-200 cursor-pointer peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100"
               >
                 <div className="block">
                   <div className="w-full">Đã sử dụng</div>
@@ -196,10 +210,10 @@ export default function AddItemForm() {
             </li>
           </ul>
 
-          <div class="mb-6">
+          <div className="mb-6">
             <label
-              for="price"
-              class="block mb-2 text-sm font-medium text-gray-900"
+              htmlFor="price"
+              className="block mb-2 text-sm font-medium text-gray-900"
             >
               Giá
             </label>
@@ -208,7 +222,6 @@ export default function AddItemForm() {
               name="price"
               placeholder="Nhập giá tiền"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              defaultValue={1000}
               decimalsLimit={2}
               onChange={(e) => handleChange(e)}
               suffix="VND"
@@ -217,7 +230,7 @@ export default function AddItemForm() {
 
           <div className="mb-6">
             <label
-              for="header"
+              htmlFor="header"
               className="block mb-2 text-sm font-medium text-gray-900"
             >
               Tiêu đề
@@ -225,6 +238,7 @@ export default function AddItemForm() {
             <input
               type="text"
               id="header"
+              name="header"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder="Tiêu đề cho mặt hàng của bạn"
               onChange={(e) => handleChange(e)}
@@ -234,7 +248,7 @@ export default function AddItemForm() {
 
           <div className="mb-6">
             <label
-              for="desc"
+              htmlFor="desc"
               className="block mb-2 text-sm font-medium text-gray-900"
             >
               Mô tả (dưới 50 từ)
@@ -242,6 +256,7 @@ export default function AddItemForm() {
             <textarea
               id="desc"
               rows="4"
+              name="desc"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
               onChange={(e) => handleChange(e)}
               placeholder="Mô tả mặt hàng..."
@@ -250,7 +265,7 @@ export default function AddItemForm() {
 
           <div className="mb-6">
             <label
-              for="location"
+              htmlFor="location"
               className="block mb-2 text-sm font-medium text-gray-900"
             >
               Địa chỉ nhận hàng
@@ -258,35 +273,25 @@ export default function AddItemForm() {
             <input
               type="text"
               id="location"
+              name="addr"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="Ký túc xá Khu A"
+              placeholder="268, Lý Thường Kiệt. Phường 14, Quận 10"
               onChange={(e) => handleChange(e)}
               required
             />
           </div>
 
           <div className="inline-flex">
-            <button className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 mr-5 rounded">
-              Đăng tin
-            </button>
-            <a
-              href="#"
-              className="bg-red-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
+            <button
+              type="submit"
+              className="bg-blue-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-10 mr-5 rounded"
             >
-              Hủy thêm
-            </a>
+              Tiếp tục
+            </button>
           </div>
         </form>
         <ToastContainer />
       </div>
     </div>
   );
-}
-
-const toastOptions = {
-  position: "top-right",
-  autoClose: 2000,
-  pauseOnHover: true,
-  draggable: true,
-  theme: "dark"
 }
--- client/src/components/pages/DisplayItem.js
+++ client/src/components/pages/DisplayItem.js
@@ -2,59 +2,59 @@ import React from "react";
 
 export default function DisplayItem() {
   return (
-    <div className="flex bg-gray-300 py-5 px-60">
+    <div className="flex bg-gray-300 border px-60">
       <div className=" bg-white px-10 py-10 leading-loose">
         <div
           id="carouselExampleControls"
           className="mb-10 w-1/4 carousel slide relative"
           data-bs-ride="carousel"
         >
-          <div class="carousel-inner relative w-full overflow-hidden">
-            <div class="carousel-item active relative float-left w-full">
+          <div className="carousel-inner relative w-full overflow-hidden">
+            <div className="carousel-item active relative float-left w-full">
               <img
                 src="https://www.lib.hcmut.edu.vn/uploads/noidung/giao-trinh-giai-tich-1-0-830.jpg"
-                class="block w-full"
+                className="block w-full"
                 alt="Wild Landscape"
               />
             </div>
-            <div class="carousel-item relative float-left w-full">
+            <div className="carousel-item relative float-left w-full">
               <img
                 src="https://www.lib.hcmut.edu.vn/uploads/noidung/giao-trinh-giai-tich-1-0-830.jpg"
-                class="block w-full"
+                className="block w-full"
                 alt="Camera"
               />
             </div>
-            <div class="carousel-item relative float-left w-full">
+            <div className="carousel-item relative float-left w-full">
               <img
                 src="https://www.lib.hcmut.edu.vn/uploads/noidung/giao-trinh-giai-tich-1-0-830.jpg"
-                class="block w-full"
+                className="block w-full"
                 alt="Exotic Fruits"
               />
             </div>
           </div>
           <button
-            class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+            className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
             type="button"
             data-bs-target="#carouselExampleControls"
             data-bs-slide="prev"
           >
             <span
-              class="carousel-control-prev-icon inline-block bg-no-repeat"
+              className="carousel-control-prev-icon inline-block bg-no-repeat"
               aria-hidden="true"
             ></span>
-            <span class="visually-hidden">Previous</span>
+            <span className="visually-hidden">Previous</span>
           </button>
           <button
-            class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+            className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
             type="button"
             data-bs-target="#carouselExampleControls"
             data-bs-slide="next"
           >
             <span
-              class="carousel-control-next-icon inline-block bg-no-repeat"
+              className="carousel-control-next-icon inline-block bg-no-repeat"
               aria-hidden="true"
             ></span>
-            <span class="visually-hidden">Next</span>
+            <span className="visually-hidden">Next</span>
           </button>
         </div>
 
