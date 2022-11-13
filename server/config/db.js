13cfd5db72266ab5bc313d28212687eace60b918
duy1711ak <duy.pham1711a@hcmut.edu.vn>
2022-11-13 20:16:38 +0700

    connect database

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,16 @@
-//connect database
+const mongoose = require('mongoose');
+
+const connectDB = () => {
+    const uri = process.env.DBCONNECT_URI;
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('Connected to DB');
+        })
+        .catch(
+            err => {
+                console.log('err', err);
+            }
+        );
+}
+
+module.exports = connectDB
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(cors())
 
@@ -9,3 +12,6 @@ app.get('/api', (req, res) => {
 })
 
 app.listen(5000, () => { console.log("Server started on 5000"); })
+
+const connectDB = require("./config/db");
+connectDB();
