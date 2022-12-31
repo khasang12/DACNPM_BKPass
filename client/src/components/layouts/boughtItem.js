795cf294ced5c8679c9ea7eb0f73eb8a78fea5aa
phii-1003 <phi.bui1003yurie@hcmut.edu.vn>
2022-12-31 11:05:24 +0700

    Edit UI đang bán

--- client/src/components/layouts/boughtItem.js
+++ client/src/components/layouts/boughtItem.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
-// import { timeAgo } from "../../../service/timeAgo";
-import { useNavigate } from "react-router-dom";
 
 const BoughtItem = ({ item }) => {
-  const navigate = useNavigate();
   const handleSubmit = (event) => {
     if (event.target.name === "comment") {
-      navigate("/add-comment");
-    } else if (event.target.name === "redo") {
-      navigate("/demo-item");
+      window.location.assign(`./user/${item.idAuthor}/add-comment`);
+    } else if (event.target.name === "toAcc") {
+      window.location.assign(`./user/${item.idAuthor}`);
     }
   };
+  // const findUserById = (id) => {
+  //   const key = Object.keys(users.users).find(user => users.users[user].id === '1')
+  //   return users.users[key]
+  // }
+
 
   return (
-  <div className="max-w-[700px] w-full flex flex-col border-2 m-1 py-2 border-[#1488D8] rounded-2 relative">
+  <div className="max-w-[700px] w-full flex flex-col border-b-2 md:border-2 m-1 py-2 border-[#1488D8] rounded-2 relative">
     <div className="w-full mb-1 text-black font-bold indent-2 border-b-2">
-      {item.title}
+      {item.idAuthor}
     </div>
     <div className="w-full md:grid md:grid-cols-2" style={{"gridTemplateColumns":"4fr 1fr"}}>
       <div className="w-full flex flex-row">
@@ -27,16 +29,16 @@ const BoughtItem = ({ item }) => {
           />
         </div>
         <div className="flex flex-col">
-          <div className="mb-1 h-1/2" style={{minHeight: "50px"}}>{item.name}</div>
+          <div className="pb-2 h-1/2 text-[20px]" style={{minHeight: "28px"}}>{item.title}</div>
           <div className="flex flex-row text-lg text-[#030981] h-3/10 relative"> 
-            <div className="text-left font-semibold">
+            <div className="text-left font-semibold text-[16px]">
               {"Giá: "}
               {item.price}
             </div>
           </div>
           <div className="text-sm text-[#1488D8] font-semibold h-2/10">
             {"Đã quan tâm lúc "}
-            {/* {<Date />} */}
+            {}
           </div>
         </div>
         <div className="flex flex-row relative">
@@ -47,7 +49,7 @@ const BoughtItem = ({ item }) => {
       </div>
       <div className="w-full relative hidden md:flex md:flex-col">
         <button 
-          name = ""
+          name = "toAcc"
           onClick={handleSubmit} 
           className='bg-[#1488D8] text-white right-0 w-[120px] rounded-md font-medium my-1 mx-auto md:mx-0 px-1 py-1'>Liên hệ</button>
         <button 
@@ -56,9 +58,9 @@ const BoughtItem = ({ item }) => {
           className='bg-[#E95959] text-white right-0 w-[120px] rounded-md font-medium my-1 mx-auto md:mx-0 px-1 py-1'>Đánh giá</button>
         </div>
     </div>
-    <div className="w-full flex align-middle justify-items-end md:hidden">
+    <div className="w-full flex flex-row items-start md:hidden">
       <button 
-        name = ""
+        name = "toAcc"
         onClick={handleSubmit} 
         className='bg-[#1488D8] text-white right-0 w-[120px] rounded-md font-medium my-1 mx-auto py-1'>Liên hệ</button>
       <button 
